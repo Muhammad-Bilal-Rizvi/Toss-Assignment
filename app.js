3f434b5114c69c5e4995df0c93ddd1309e346737
Muhammad Bilal Rizvi <bilal742742@gmail.com>
2024-03-13 01:17:18 +0500

    toss assignment

---
+++ app.js
@@ -0,0 +1,23 @@
+//0 == chand
+//1 == masjid
+const image = document.querySelector('#coin-image');
+const h1 = document.querySelector('h1');
+// image.src = 'https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-18814.jpg'
+// console.log(image);
+
+function toss(user) {
+    const randomNumber = Math.floor(Math.random() * 2);
+    if(randomNumber === 0){
+        image.src = 'https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-18814.jpg'
+    }else{
+        image.src = 'https://en.numista.com/catalogue/photos/pakistan/424-original.jpg'
+    }
+
+    if(user === 'chand' && randomNumber === 0 || user === 'masjid' && randomNumber === 1 ){
+        h1.innerHTML = 'User win the toss';
+
+
+    }else{
+        h1.innerHTML = 'User loose the toss';
+    }
+}
